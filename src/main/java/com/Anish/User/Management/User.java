835ab0f442a33543e29feb81a59da8efcac491a6
Anish Kumar Sharma <anish.kumar20071998@gmail.com>
2023-09-15 21:21:50 +0530

    "Readme.md is Created in this Project"

--- src/main/java/com/Anish/User/Management/User.java
+++ src/main/java/com/Anish/User/Management/User.java
@@ -7,6 +7,8 @@ public class User {
     private String address;
     private String number;
 
+    private String phooneNumber;
+
     public User(Integer userId, String name, String userName, String address, String number) {
         this.userId = userId;
         this.name = name;
